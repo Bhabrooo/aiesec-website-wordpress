413ce7d52e79770f7b4765481a9cc90a156e1bec
Eyana <eyana.mallari@obf.ateneo.edu>
2014-08-18 21:18:44 +0800

    Mental note: sub-menus, each join-aiesec should have a separate page

---
+++ wp-content/themes/aiesec-website/template-join-aiesec.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Template Name: Join AIESEC
+ *
+ *
+ */
+
+get_header(); ?>
+
+   <!-- Header -->
+    <header data-autoheight="true" style="background: url('<?php the_field('join_aiesec_background_image'); ?>') center center">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="intro-text">
+                        <span class="name">Student</span>
+                        <span class="skills">Leadership Programs blah blah</span><br>
+                        <button class="btn btn-lg btn-outline" id="header-cta">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <section id="about" class="danger">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-12 text-center heading">
+                    <h2>Global Talent</h2>
+                    <span class="subheading">
+                    </span>
+                    
+                </div>
+
+                <div class="col-sm-7 text-center">
+    
+                    <h3>Youth Org</h3>
+                    <p>Hey AIESEC! All AIESECers from Asia Pacific are invited to register for APXLDS: the biggest and funnest conference in the region</p>
+                </div><!--col-->
+
+                <div class="col-sm-5 text-center">
+                    <img src="img/portfolio/game.png" class="img-responsive" alt="">
+                </div><!--col-->
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section class="warning">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-12 text-center">
+                    <h2>Global Citizen</h2>
+                    <br><br>
+                </div>
+
+                <div class="col-sm-5 text-center">
+                    <img src="img/portfolio/circus.png" class="img-responsive" alt="">
+                </div><!--col-->
+
+                <div class="col-sm-7 text-center">
+    
+                    <h3>Youth Org</h3>
+                    <p>Hey AIESEC! All AIESECers from Asia Pacific are invited to register for APXLDS: the biggest and funnest conference in the region</p>
+                </div><!--col-->
+
+
+                </div><!--col-->
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section class="yellow">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-12 text-center">
+                    <h2>Global Leader</h2>
+                    <br><br>
+                </div>
+
+                <div class="col-sm-7 text-center">
+    
+                    <h3>Youth Org</h3>
+                    <p>Hey AIESEC! All AIESECers from Asia Pacific are invited to register for APXLDS: the biggest and funnest conference in the region</p>
+                </div><!--col-->
+
+                <div class="col-sm-5 text-center">
+                    <img src="img/portfolio/circus.png" class="img-responsive" alt="">
+                </div><!--col-->
+
+
+
+
+                </div><!--col-->
+            </div>
+        </div>
+    </section>
+ 
+
+<?php get_footer(); ?>
