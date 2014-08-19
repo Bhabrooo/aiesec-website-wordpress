deb76e5ceb584d3eac2899b486b81c949687e995
Eyana <eyana.mallari@obf.ateneo.edu>
2014-08-19 20:41:50 +0800

    added customized map for aiesec phes yay

--- wp-content/themes/aiesec-website/template-home.php
+++ wp-content/themes/aiesec-website/template-home.php
@@ -148,7 +148,7 @@ get_header(); ?>
         
     </div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
     <a class="google-map-code" href="http://www.sparmichl.de" id="get-map-data">www.sparmichl.de</a></div>
-    <script type="text/javascript"> function init_map(){var myOptions = {zoom:18,center:new google.maps.LatLng(14.646696275794682,121.07398918584443),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(14.646696275794682, 121.07398918584443)});infowindow = new google.maps.InfoWindow({content:"<b>Unit 524, Elizabeth Hall</b><br/>Katipunan Avenue, Loyola Heights, Quezon City<br/> Quezon City" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+    <script type="text/javascript"> function init_map(){var myOptions = {zoom:18,center:new google.maps.LatLng(14.6343154,121.0724134),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(14.6343154,121.0724134)});infowindow = new google.maps.InfoWindow({content:"<b>Unit 102 Liberty Place</b>, 96 Xavierville Avenue,<br/>Katipunan Avenue, Loyola Heights, Quezon City<br/> Quezon City" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
 </div>  
 </section>
 <?php get_footer(); ?>
